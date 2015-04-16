d108835d57f403f35423e95e668920e8ffce2d84
Nima <behrn916@newschool.edu>
2015-04-15 20:48:06 -0400

    lpush now

--- app.js
+++ app.js
@@ -3,10 +3,34 @@ var redis = require("redis");
 var app = express();
 var client = redis.createClient();
 
-app.get("/", function(req, res){
-	client.incr("visitors", function(err, reply){
-	res.send("you are visitor number" + reply);
 
+// lpush adds one item to the list
+
+app.get("/", function(req, res){
+	client.lpush("visitors", req.ip, function(err, reply){
+	res.send("you are visitor number " + 
+		reply +
+		" and your IP is "
+		+ req.ip);
+
+	});
+});
+
+// llen only shows the length of the list
+
+app.get("/how-many", function(req, res){
+	client.llen("visitors", function(err, visitorCount){
+	res.send("there are  " + 
+		visitorCount +
+		" right now ")
+	});
+});
+
+// llen only shows the length of the list
+
+app.get("/most-recent", function(req, res){
+	client.lindex("visitors", 0, function(err, mostResent){
+	res.send("the most recent was" + mostResent);
 	});
 });
 
