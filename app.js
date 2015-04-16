33349d73bc2ba8adbae2841bf06f3209eb5e3b41
Nima <behrn916@newschool.edu>
2015-04-15 20:10:56 -0400

    works

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require("express");
+var redis = require("redis");
 var app = express();
+var client = redis.createClient();
 
 app.get("/", function(req, res){
-	res.send("It works!");
+	client.incr("visitors", function(err, reply){
+	res.send("you are visitor number" + reply);
 
+	});
 });
 
 app.listen(3000);
